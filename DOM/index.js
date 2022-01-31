fb91367e9d81e3e43acecf7136e0dfdc88948dce
Joe <joeboy229@gmail.com>
2022-02-01 00:34:52 +0800

    The Separation of Concerns: Structure vs Style vs Behaviour

--- DOM/index.js
+++ DOM/index.js
@@ -1,3 +1,4 @@
+// 143. Selecting HTML Elements with Javascript
 document.querySelector("a");              // <a href="https://www.google.com">Google</a>
 document.querySelector("li a");           // <a href="https://www.google.com">Google</a>
 document.querySelector("#list a");        // <a href="https://www.google.com">Google</a>
@@ -11,3 +12,12 @@ document.querySelector("li a").style.color = "red";    // it change the color of
 document.getElementsByTagName("li")[2].style.color = "purple";    // change 'Third' text color to purple
 document.querySelectorAll("#list .item")[1].style.color = "blue"; // change 'Second' text color to blue
 document.getElementsByClassName("btn");   // get whole complete HTMLCollection array, so that you cannot use this way to change properties
+
+// 144. Manipulating and Changing Styles of HTML Elements with Javascript
+document.querySelector("h1").style.color = "green";   // change the color of h1 tag
+document.querySelector("h1").style.fontSize = "6rem"; // change the font size of h1 tag
+
+// 145. The Separation of Concerns: Structure vs Style vs Behaviour
+document.querySelector("button").classList.add("invisible");    // hide all buttons by adding invisible CSS code
+document.querySelector("button").classList.remove("invisible"); // show hidden buttons
+document.querySelector("button").classList.toggle("invisible"); // If buttons are invisible, then show them. Otherwise, hide them.
