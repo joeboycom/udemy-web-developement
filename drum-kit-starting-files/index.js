e142ce9ff7efd7c7aa31d7fe420ee0ddcf76062c
Joe <joeboy229@gmail.com>
2022-02-03 17:56:59 +0800

    add clicking events on each button by for-loop

--- drum-kit-starting-files/index.js
+++ drum-kit-starting-files/index.js
@@ -1,4 +1,8 @@
-document.querySelector("button").addEventListener("click", function() {
-  alert("Click");
-  // What to do when click detected.
-});
+var numberOfDrimButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrimButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    alert("Click");
+    // What to do when click detected.
+  });
+}
