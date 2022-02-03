58076926622df2e82bde9b51be8d18a7d5cbad5c
Joe <joeboy229@gmail.com>
2022-02-03 18:37:18 +0800

    Higher-Order Function and Chrome's debugger

--- drum-kit-starting-files/index.js
+++ drum-kit-starting-files/index.js
@@ -6,3 +6,29 @@ for (var i = 0; i < numberOfDrimButtons; i++) {
     // What to do when click detected.
   });
 }
+
+
+/* Higher Order Functions - Hiher order functions aere functions that can take other functions as inputs. */
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function calculator(number1, number2, operator) {
+  return operator(number1, number2);
+}
+// ex:ı
+// debugger; // make Chrome into debug mode
+// calculator(3, 4, multiple);
+// 12 // result
