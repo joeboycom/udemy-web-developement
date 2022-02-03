67eb671c52e876d84d12ce1e975468e4e544ea44
Joe <joeboy229@gmail.com>
2022-02-04 02:31:45 +0800

    use switch case to play different drum sounds by clicking different buttons

--- drum-kit-starting-files/index.js
+++ drum-kit-starting-files/index.js
@@ -2,15 +2,50 @@ var numberOfDrimButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrimButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() { // type = "click"
-    console.log(this);            // <button class="d drum">d</button>
-    console.log(this.innerHTML);  // d
-    console.log(this.style.color = "white");         
+    console.log(this); // <button class="d drum">d</button>
+    console.log(this.innerHTML); // d
+    console.log(this.style.color = "white");
+
+    var buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML) {
+      case 'w':
+        playAudio("sounds/tom-1.mp3");
+        break;
+
+      case 'a':
+        playAudio("sounds/tom-2.mp3");
+        break;
+
+      case 's':
+        playAudio("sounds/tom-3.mp3");
+        break;
+
+      case 'd':
+        playAudio("sounds/tom-4.mp3");
+        break;
+      case 'j':
+        playAudio("sounds/snare.mp3");
+        break;
+
+      case 'k':
+        playAudio("sounds/crash.mp3");
+        break;
+
+      case 'l':
+        playAudio("sounds/kick-bass.mp3");
+        break;
+
+      default: console.log(buttonInnerHTML);
+
+    }
+
   });
 }
 
-
-// var audio = new Audio("sounds/crash.mp3");
-// audio.play();
+function playAudio(path) {
+  var audio = new Audio(path);
+  audio.play();
+}
 
 /* Higher Order Functions - Hiher order functions aere functions that can take other functions as inputs. */
 function add(num1, num2) {
