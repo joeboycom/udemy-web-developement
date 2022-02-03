ae510c4590ab2efbea9e8ab610a309fef65e73c2
Joe <joeboy229@gmail.com>
2022-02-04 02:40:33 +0800

    detecting keyboard press and make sound

--- drum-kit-starting-files/index.js
+++ drum-kit-starting-files/index.js
@@ -7,45 +7,53 @@ for (var i = 0; i < numberOfDrimButtons; i++) {
     console.log(this.style.color = "white");
 
     var buttonInnerHTML = this.innerHTML;
-    switch (buttonInnerHTML) {
-      case 'w':
-        playAudio("sounds/tom-1.mp3");
-        break;
-
-      case 'a':
-        playAudio("sounds/tom-2.mp3");
-        break;
-
-      case 's':
-        playAudio("sounds/tom-3.mp3");
-        break;
-
-      case 'd':
-        playAudio("sounds/tom-4.mp3");
-        break;
-      case 'j':
-        playAudio("sounds/snare.mp3");
-        break;
-
-      case 'k':
-        playAudio("sounds/crash.mp3");
-        break;
-
-      case 'l':
-        playAudio("sounds/kick-bass.mp3");
-        break;
-
-      default: console.log(buttonInnerHTML);
-
-    }
-
+    makeSound(buttonInnerHTML);
   });
+}
+
+function makeSound(key) {
+  switch (key) {
+    case 'w':
+      playAudio("sounds/tom-1.mp3");
+      break;
+
+    case 'a':
+      playAudio("sounds/tom-2.mp3");
+      break;
+
+    case 's':
+      playAudio("sounds/tom-3.mp3");
+      break;
+
+    case 'd':
+      playAudio("sounds/tom-4.mp3");
+      break;
+    case 'j':
+      playAudio("sounds/snare.mp3");
+      break;
+
+    case 'k':
+      playAudio("sounds/crash.mp3");
+      break;
+
+    case 'l':
+      playAudio("sounds/kick-bass.mp3");
+      break;
+
+    default: console.log(buttonInnerHTML);
+
+  }
 }
 
 function playAudio(path) {
   var audio = new Audio(path);
   audio.play();
 }
+
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+})
+
 
 /* Higher Order Functions - Hiher order functions aere functions that can take other functions as inputs. */
 function add(num1, num2) {
