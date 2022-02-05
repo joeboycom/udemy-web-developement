eb914dc7a8d88ccc234b2bade319bc708beccc8f
Joe <joeboy229@gmail.com>
2022-02-05 16:08:03 +0800

    The NPM Package Manager and Installing External Node Modules - superheroes module

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,4 +1,12 @@
 // jshint esversion:6
-const fs = require("fs");
 
-fs.copyFileSync("file1.txt", "file2.txt");
+// const fs = require("fs");
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+// npm install superheroes
+// node index.js
+const superheroes = require('superheroes');
+var heros = superheroes.all;  //=> ['3-D Man', 'A-Bomb', …]
+var hero = superheroes.random(); //=> 'Spider-Ham'
+console.log(heros);
+console.log(hero);
