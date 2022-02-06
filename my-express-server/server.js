59f8ac0d46e7a249ff73e739ccb4c5d0cd3c22be
Joe <joeboy229@gmail.com>
2022-02-06 23:53:25 +0800

    Handling Requests and Responses: the GET Request

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -4,7 +4,11 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000
+const port = 3001;
+
+app.get("/", function(request, response) {
+  response.send("Hello");
+});
 
 app.listen(port, function() {
   console.log("Server started on port 3000");
