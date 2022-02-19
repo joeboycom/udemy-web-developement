15cba195ef123d5356983afa5f9829ff84e656e7
Joe <joeboy229@gmail.com>
2022-02-19 20:54:11 +0800

    Using Body Parser to Parse POST Requests to the Server

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -2,12 +2,29 @@
 
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res) {
+app.use(bodyParser.urlencoded({extended: true}));
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=499981c60f06e6f25a39cdbe21601753&units=metric";
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html")
+
+  // res.send("Server is up and running."); // We can only have one res.send in any given one of these app methods
+});
+
+app.post("/", function(req, res) {
+  console.log(req.body.cityName);
+  getWeather(req.body.cityName, res);
+  console.log("Post request received");
+});
+
+function getWeather(query, res) {
+  // const query = "London";
+  const appid = "499981c60f06e6f25a39cdbe21601753";
+  const units = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + appid+ "&units=" + units;
 
   https.get(url, function(response) {
     console.log(response.statusCode);
@@ -26,16 +43,13 @@ app.get("/", function(req, res) {
       console.log(temp); // 7.7
       console.log(weatherDescription); // moderate rain
 
-
-
       res.write("<p>The weather is currently " + weatherDescription + "</p>");
       res.write("<h1>The temperature is " + temp + " degress Celcius</h1>");
       res.write("<img src=" + imageURL + ">");
       res.send();
     });
   });
-  // res.send("Server is up and running."); // We can only have one res.send in any given one of these app methods
-});
+}
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
