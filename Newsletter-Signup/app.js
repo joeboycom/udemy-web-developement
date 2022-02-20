6053b22a301e549c4b10223f2d07936cc6c8e7c8
Joe <joeboy229@gmail.com>
2022-02-21 00:21:16 +0800

    allows you to use static files in public folder

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -6,6 +6,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// allows you to use static files in public folder
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
 });
