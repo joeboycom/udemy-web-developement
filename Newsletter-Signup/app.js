fd9abbb3c8a5e550a0f3a83f5253d03733677820
Joe <joeboy229@gmail.com>
2022-02-26 20:56:22 +0800

    Setting up the Sign Up page and receive data from request body

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -3,15 +3,27 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
 // allows you to use static files in public folder
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
+
+app.post("/", function(req, res){
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
+});
+
+
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
 });
