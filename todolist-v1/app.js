3cb2655bd75754f55f3adbb8348059b5258d4b85
Joe <joeboy229@gmail.com>
2022-02-28 01:50:34 +0800

    Add basic code and ready to use template

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,8 +5,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.get("/", function(request, response) {
-  response.send("<h1>Hello world</h1>");
+var today = new Date();
+var currentDay = today.getDay()
+
+app.get("/", function(req, res) {
+
+  if (currentDay ===6 || currentDay ===0) {
+    res.write("<h1>Yay it's the weekend!</h1>");
+  } else {
+    res.sendFile(__dirname + "/index.html");
+  }
 });
 
 
